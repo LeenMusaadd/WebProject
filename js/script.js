b1df6f8714c9374d7c4fa970e99274fff862335b
Leen Musaad <LeenMusaadd@gmail.com>
2024-10-04 20:12:25 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const loginIcon = document.getElementById('img2');
+//     const messageBox = document.querySelector('.hover-message');
+
+//     // Show the message box on mouse enter
+//     loginIcon.addEventListener('mouseenter', () => {
+//         messageBox.style.display = 'block';
+//     });
+
+//     // Hide the message box on mouse leave
+//     loginIcon.addEventListener('mouseleave', () => {
+//         messageBox.style.display = 'none';
+//     });
+// });
